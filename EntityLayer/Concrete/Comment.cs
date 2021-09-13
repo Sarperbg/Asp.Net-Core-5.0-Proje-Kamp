92cded45040654a13d2159ff900eb63c03075302
example <sarpergulfirat@hotmail.com>
2021-09-13 12:57:01 +0300

    #8 Add project files.

--- EntityLayer/Concrete/Comment.cs
+++ EntityLayer/Concrete/Comment.cs
@@ -10,6 +10,10 @@ namespace EntityLayer.Concrete
     {
         public int CommentID { get; set; }
         public string CommentUserName { get; set; }
+        public string CommentTitle { get; set; }
+        public string CommentContent { get; set; }
+        public DateTime CommentDate { get; set; }
+        public bool CommentStatus { get; set; }
 
     }
 }
